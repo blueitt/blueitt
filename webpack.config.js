5528f82f7d5ff7b75d45f72ec02325fd992a52ea
Ulysse Carion <ulysse@ulysse.io>
2016-12-22 10:56:41 -0800

    Add foundation and tell webpack how to load it

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,17 @@ module.exports = {
         new webpack.NoErrorsPlugin(),
     ],
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['babel'],
-            include: path.join(__dirname, 'src')
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: path.join(__dirname, 'src'),
+            },
+        ]
     }
 };
