6e2e7232dbd43b587a6df659e73958227d5bb238
Ulysse Carion <ulysse@ulysse.io>
2017-01-11 15:05:46 -0800

    Make css hot-loading work

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-var path = require('path');
-var webpack = require('webpack');
-var express = require('express');
-var devMiddleware = require('webpack-dev-middleware');
-var hotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
+const path = require('path');
+const webpack = require('webpack');
+const express = require('express');
+const devMiddleware = require('webpack-dev-middleware');
+const hotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
 
-var app = express();
-var compiler = webpack(config);
+const app = express();
+const compiler = webpack(config);
 
 app.use(devMiddleware(compiler, {
+    hot: true,
     publicPath: config.output.publicPath,
     historyApiFallback: true,
 }));
 
 app.use(hotMiddleware(compiler));
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function (err) {
+app.listen(3000, (err) => {
     if (err) {
         return console.error(err);
     }
